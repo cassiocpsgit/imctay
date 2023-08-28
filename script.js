e3a321d486f8fc7bbd6cf87f06a7a9284d3eca7e
cassiocpsgit <83365369+cassiocpsgit@users.noreply.github.com>
2023-08-28 13:56:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function calcular() {
     let peso = document.getElementById('ipeso')
     let alt = document.getElementById('ialtu')
     let res = document.querySelector('div#res')
-    let imc = Number(peso.value) / Number(alt.value * 2)
+    let imc = Number(peso.value) / Number(alt.value ** 2)
     if (Number(peso.value.length) == 0 || Number(alt.value.length) == 0){
         window.alert('Digite os valores para calcular')
     } else {
-        res.innerHTML = `IMC = ${imc.toFixed(1)}`
+        res.innerHTML = `IMC = ${imc.toFixed(2)}Kg/m²`
     }
 }
